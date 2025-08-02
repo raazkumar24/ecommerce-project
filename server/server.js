81cb4a0e21c78b7815d8ec7b1f8ee05cc586d1c1
Raj Shekhar <raazverma625@gmail.com>
2025-08-02 12:00:14 +0530

    update

--- server/server.js
+++ server/server.js
@@ -197,10 +197,10 @@
 //     await connectDB();
 //     // Then, start the Express server
 //     app.listen(PORT, () => {
-//       console.log(`✅ Server is running on port ${PORT}`);
+//       console.log(` Server is running on port ${PORT}`);
 //     });
 //   } catch (error) {
-//     console.error(`❌ Failed to start server:`, error);
+//     console.error(` Failed to start server:`, error);
 //     process.exit(1); // Exit the process with a failure code
 //   }
 // };
